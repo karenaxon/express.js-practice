6081a38f74de47fda78f1db444005d84fd3bc9be
Kaxon <karenaxon@users.noreply.github.com>
2022-06-21 10:41:16 -0700

    Add full CRUD to users.js

--- routes/users.js
+++ routes/users.js
@@ -10,5 +10,17 @@ router.get("/new", (req, res) => {
   res.send("New User Form")
 })
 
+router
+  .route("/:id")
+  .get((req, res) => {
+    res.send(`Get User with ID ${req.params.id}`)
+  })
+  .put((req, res) => {
+    res.send(`Update User with ID ${req.params.id}`)
+  })
+  .delete((req, res) => {
+    res.send(`Delete Use with ID ${req.params.id}`)
+  })
+
 module.exports = router
 
