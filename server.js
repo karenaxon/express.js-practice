83194941b7a537672ca41fced0e360c162e1c246
Kaxon <karenaxon@users.noreply.github.com>
2022-06-20 20:21:38 -0700

    Add server file

---
+++ server.js
@@ -0,0 +1,16 @@
+const { render } = require('ejs');
+const express = require('express');
+const app = express();
+
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+  // res.json({ message: "Hello!!!" });
+  // res.download("server.js")
+  res.render("index", { text: "World"})
+})
+
+const userRouter = require("./routes/users");
+app.use("/users", userRouter);
+
+app.listen(3000);
